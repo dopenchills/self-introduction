d1c567d679e963c95f8b48e1a6fd343cff433bad
dopenchills <dopenchills@gmail.com>
2022-11-19 21:38:36 +0900

    feat: add style to PreferenceIconList

--- src/components/PreferenceIconList.tsx
+++ src/components/PreferenceIconList.tsx
@@ -1,12 +1,13 @@
 import * as React from "react"
 import { FaRegMoon } from "@react-icons/all-files/fa/FaRegMoon";
 import { FaGlobe } from "@react-icons/all-files/fa/FaGlobe";
+import './PreferenceIconList.scss'
 
 const PreferenceIconList = (props: {className?: string}): JSX.Element => {
   return (
     <ul className={`preference-icon-list ${props.className}`}>
-      <li className="preference-icon"><FaRegMoon /></li>
-      <li className="preference-icon"><FaGlobe /></li>
+      <li className="preference-icon-item"><FaRegMoon className="preference-icon" /></li>
+      <li className="preference-icon-item"><FaGlobe className="preference-icon" /></li>
     </ul>
   )
 }
