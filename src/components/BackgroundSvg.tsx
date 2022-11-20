6f03c7ca504ac83c68ba759c63177e1ddf0732a1
dopenchills <dopenchills@gmail.com>
2022-11-20 23:43:32 +0900

    feat: add animation to all the clouds

--- src/components/BackgroundSvg.tsx
+++ src/components/BackgroundSvg.tsx
@@ -12,7 +12,74 @@ import CloudSmall from '-!svg-react-loader?name=Background!../assets/CloudSmall.
 import './BackgroundSvg.scss'
 
 
-const BackgroundSvg = (props: {className?: string}): JSX.Element => {
+const Clouds = (): JSX.Element => {
+  const animatedCloudItems = [
+    {
+      id: "cloud-big",
+      className: "cloud cloud-big",
+      style: {top: 100},
+      element: <CloudBig width={300} height={100} />,
+      nodeRef: useRef(null),
+      inState: React.useState(false),
+      timeout: {enter: 240000, exit: 10}
+    },
+    {
+      id: "cloud-medium",
+      className: "cloud cloud-medium",
+      style: {top: 100},
+      element: <CloudMedium width={200} height={100} />,
+      nodeRef: useRef(null),
+      inState: React.useState(false),
+      timeout: {enter: 480000, exit: 10}
+    },
+    {
+      id: "cloud-small",
+      className: "cloud cloud-small",
+      style: {top: 100},
+      element: <CloudSmall width={100} height={100} />,
+      nodeRef: useRef(null),
+      inState: React.useState(false),
+      timeout: {enter: 960000, exit: 10}
+    }
+  ]
+
+  React.useEffect(() => {
+    animatedCloudItems.map(item => {
+      const setIn = item.inState[1]
+      setIn(true)
+    })
+  }, [])
+
+  return (
+    <>
+      {
+        animatedCloudItems.map(({id, className, style, element, nodeRef, inState, timeout}) => {
+          const isIn  = inState[0]
+          const setIn = inState[1]
+          return (
+            <CSSTransition
+              key={id}
+              nodeRef={nodeRef}
+              timeout={{enter: timeout.enter, exit: timeout.exit}}
+              classNames="cloud"
+              onEntered={() => {console.log("entered"); setTimeout(() => {setIn(false)}, 100)}}
+              onExited={()  => {console.log("exited");  setTimeout(() => {setIn(true)},  100)}}
+              in={isIn}
+              mountOnEnter
+              unmountOnExit
+            >
+              <div className={className} style={style} ref={nodeRef}>
+                {element}
+              </div>
+            </CSSTransition>
+          )
+        })
+      }
+    </>
+  )
+}
+
+const ObjectsOnGround = (): JSX.Element => {
   const [windowSize, setWindowSize] = React.useState({width: window.innerWidth, height: window.innerHeight})
 
   const resize = () => {
@@ -34,31 +101,21 @@ const BackgroundSvg = (props: {className?: string}): JSX.Element => {
   const verticalOffsetMargin = 5  // in order to hide white area under the svg
   const verticalOffset   = windowSize.height - svgRenderHeight + verticalOffsetMargin
 
-  // for animation
-  const nodeRef = useRef(null)
-  const [inProp, setInProp] = React.useState(false)
-  React.useEffect(() => {
-    setInProp(true)
-  }, [])
+  return (
+      <Background
+      className="background-svg"
+      width={windowSize.width}
+      height={svgRenderHeight}
+      style={{top: verticalOffset}}
+    />
+  )
+}
+
+const BackgroundSvg = (props: {className?: string}): JSX.Element => {
   return (
     <div className={`background-div ${props.className}`}>
-      <CSSTransition nodeRef={nodeRef} in={inProp} timeout={{appear: 0, enter: 10000, exit: 0}} classNames="cloud"
-        onEntered={() => {setTimeout(() => {setInProp(false)}, 10000)}}
-        onExited={() => {setTimeout(() => {setInProp(true), 10})}}
-        unmountOnExit
-      >
-        <div className="cloud cloud-big" ref={nodeRef} style={{top: 50}}>
-          <CloudBig width={300} height={100} />
-        </div>
-      </CSSTransition>
-      <CloudMedium className="cloud cloud-medium" width={200} height={100} style={{top: 100}}  />
-      <CloudSmall className="cloud cloud-small" width={100} height={100} style={{top: 200}}  />
-      <Background
-        className="background-svg"
-        width={windowSize.width}
-        height={svgRenderHeight}
-        style={{top: verticalOffset}}
-      />
+      <Clouds />
+      <ObjectsOnGround />
     </div>
   )
 }
