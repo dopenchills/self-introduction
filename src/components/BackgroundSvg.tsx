71f15bda7910a30413e38474f1acee1df042afb8
dopenchills <dopenchills@gmail.com>
2022-11-20 19:41:43 +0900

    feat: add clouds to background

--- src/components/BackgroundSvg.tsx
+++ src/components/BackgroundSvg.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import Background from '-!svg-react-loader?name=Background!../assets/Background.svg'
-
+import CloudBig from '-!svg-react-loader?name=Background!../assets/CloudBig.svg'
+import CloudMedium from '-!svg-react-loader?name=Background!../assets/CloudMedium.svg'
+import CloudSmall from '-!svg-react-loader?name=Background!../assets/CloudSmall.svg'
+import './BackgroundSvg.scss'
 
 const BackgroundSvg = (props: {className?: string}): JSX.Element => {
   const [windowSize, setWindowSize] = React.useState({width: window.innerWidth, height: window.innerHeight})
@@ -23,13 +26,19 @@ const BackgroundSvg = (props: {className?: string}): JSX.Element => {
   const svgRenderHeight  = svgDefaultHeight * (windowSize.width / svgDefaultWidth)
   const verticalOffsetMargin = 5  // in order to hide white area under the svg
   const verticalOffset   = windowSize.height - svgRenderHeight + verticalOffsetMargin
+
   return (
-    <Background
-      className={`background-svg ${props.className}`}
-      width={windowSize.width}
-      height={svgRenderHeight}
-      style={{top: verticalOffset}}
-    />
+    <div className={`background-div ${props.className}`}>
+      <CloudBig className="cloud cloud-big" width={300} height={100} style={{top: Math.random() / 2 * windowSize.height}} />
+      <CloudMedium className="cloud cloud-medium" width={200} height={100} style={{top: Math.random() / 2 * windowSize.height}}  />
+      <CloudSmall className="cloud cloud-small" width={100} height={100} style={{top: Math.random() / 2 * windowSize.height}}  />
+      <Background
+        className="background-svg"
+        width={windowSize.width}
+        height={svgRenderHeight}
+        style={{top: verticalOffset}}
+      />
+    </div>
   )
 }
 
