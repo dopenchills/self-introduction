31fc5f1784af06fca7044c5de50103c48bee7988
dopenchills <dopenchills@gmail.com>
2022-11-20 11:34:39 +0900

    feat: add About

--- src/components/ContentsContainer.tsx
+++ src/components/ContentsContainer.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Home from "./contents/Home"
+import About from "./contents/About"
 
 const ContentsContainer = (props: {className?: string}): JSX.Element => {
   return (
     <main className={`contents-container ${props.className}`}>
       <Home />
+      <About />
     </main>
   )
 }
---
+++ src/components/contents/About.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const About = (): JSX.Element => {
+  return (
+    <>
+      1996年生まれ。神奈川県川崎市高津区在住。
+    </>
+  )
+}
+
+export default About
