f208c58c12b1b91f108852a5c00f8ad5d5fa9f21
dopenchills <dopenchills@gmail.com>
2022-11-21 17:13:51 +0900

    feat: add structure of Portfolio content

--- src/components/contents/Portfolio.tsx
+++ src/components/contents/Portfolio.tsx
@@ -2,9 +2,33 @@ import * as React from "react"
 
 const Portfolio = (): JSX.Element => {
   return (
-    <>
-      My Portfolio
-    </>
+    <article>
+      <section>
+        <h2>自己紹介サイト (本サイト)</h2>
+        <p>
+          概要
+        </p>
+        <h3>こだわった点</h3>
+        <ul>
+          <li>Performance</li>
+          <li>Accessibility</li>
+          <li>Fun to use</li>
+        </ul>
+        <p>
+        </p>
+        <h3>大変だった点</h3>
+        <p>
+          <ul>
+            <li>フロントエンドの知識が少なく、勉強に時間がかかった点</li>
+          </ul>
+        </p>
+        <h3>使用した技術</h3>
+        <ul>
+          <li>TypeScript</li>
+          <li>Gatsby.js</li>
+        </ul>
+      </section>
+    </article>
   )
 }
 
