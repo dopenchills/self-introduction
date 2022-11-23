a2b37dda5317c7e36cc4b08029079595a48da8b6
dopenchills <dopenchills@gmail.com>
2022-11-23 17:36:45 +0900

    feat: add animation to MenuPane

--- src/components/MenuPane.tsx
+++ src/components/MenuPane.tsx
@@ -2,22 +2,36 @@ import * as React from 'react'
 import MenuList from './MenuList'
 import './MenuPane.scss'
 import { MenuContext } from './contexts/Menu'
+import { motion, AnimatePresence } from 'framer-motion'
+
 
 const MenuPane = (props: {className?: string}): JSX.Element => {
   const { showMenuPane, dispatch } = React.useContext(MenuContext)
-  if (!showMenuPane) {
-    return <></>
-  }
 
   const onClickTransparent = () => {
     dispatch({type: 'hideMenuPane', payload: ""})
   }
 
   return (
-    <div className={`menu-pane ${props.className ? props.className : ""}`}>
-      <div className='menu-pane-transparent' onClick={onClickTransparent}></div>
-      <MenuList className='menu'/>
-    </div>
+    <AnimatePresence>
+    {
+      showMenuPane && 
+        <motion.div
+          key="menupane"
+          className={`menu-pane ${props.className ? props.className : ""}`}
+          initial={{ translateX: "10%", opacity: 0 }}
+          animate={{ translateX: 0, opacity: 1 }}
+          exit={{ translateX: "10%", opacity: 0 }}
+          transition={{
+            type: "linear",
+            duration: 0.5
+          }}
+        >
+        <div className='menu-pane-transparent' onClick={onClickTransparent}></div>
+        <MenuList className='menu'/>
+      </motion.div>
+    }
+    </AnimatePresence>
   )
 }
 
