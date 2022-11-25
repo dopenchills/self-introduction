5efe0891919f9aef9d85455a41065e90bfcc9d81
dopenchills <dopenchills@gmail.com>
2022-11-25 10:38:06 +0900

    test: remove wrong lines

--- src/components/__tests__/MenuList.tsx
+++ src/components/__tests__/MenuList.tsx
@@ -1,31 +1,26 @@
 import * as React from "react"
 import renderer from "react-test-renderer"
-import {render, fireEvent, screen} from '../../../utils/test-utils'
+import { render, screen } from '../../../utils/test-utils'
 import '@testing-library/jest-dom'
 
 import { MenuOption } from "../MenuList"
 
+
 describe(`MenuOption`, () => {
+  const name  = "menu"
+  const value = "portfolio"
+  const label = value.toUpperCase()
+
   it(`has the same snapshot`, () => {
     const component = renderer.create(
-      <MenuOption name="menu" value="portfolio" label={"portfolio".toUpperCase()} checked={true} />
+      <MenuOption name={name} value={value} label={label} checked={true} />
     )
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   it(`shows label text`, async () => {
-    const value = "portfolio"
-    const label = value.toUpperCase()
-    render(<MenuOption name="menu" value={value} label={label} checked={false} />)
+    render(<MenuOption name={name} value={value} label={label} checked={false} />)
     expect(screen.getByText(label)).toHaveTextContent(label)
-  })
-
-  it(`is checked on click`, async () => {
-    const value = "portfolio"
-    const label = value.toUpperCase()
-    render(<MenuOption name="menu" value={value} label={label} checked={false} />)
-    fireEvent.click(screen.getByText(label))
-    expect(screen.getByLabelText(label)).toBeChecked
   })
 })
