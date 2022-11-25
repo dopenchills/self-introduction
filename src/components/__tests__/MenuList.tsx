399388b6e7d12a856c267600328234dbd7a7a044
dopenchills <dopenchills@gmail.com>
2022-11-25 11:16:08 +0900

    test: add test of MenuList

--- src/components/__tests__/MenuList.tsx
+++ src/components/__tests__/MenuList.tsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 import renderer from "react-test-renderer"
-import { render, screen } from '../../../utils/test-utils'
+import { fireEvent, render, screen } from '../../../utils/test-utils'
 import '@testing-library/jest-dom'
 
-import { MenuOption } from "../MenuList"
+import MenuList, { MenuOption } from "../MenuList"
 
 
 describe(`MenuOption`, () => {
@@ -22,5 +22,22 @@ describe(`MenuOption`, () => {
   it(`shows label text`, async () => {
     render(<MenuOption name={name} value={value} label={label} checked={false} />)
     expect(screen.getByText(label)).toHaveTextContent(label)
+  })
+})
+
+describe(`MenuList`, () => {
+  it(`has home radio button that is initially checked`, () => {
+    render(<MenuList />)
+    expect((screen.getByLabelText("HOME") as HTMLInputElement).checked).toBe(true)
+    expect((screen.getByLabelText("SKILLS") as HTMLInputElement).checked).toBe(false)
+  })
+
+  it(`changed its value with onClick`, () => {
+    render(<MenuList />)
+    expect((screen.getByLabelText("HOME") as HTMLInputElement).checked).toBe(true)
+    expect((screen.getByLabelText("SKILLS") as HTMLInputElement).checked).toBe(false)
+    fireEvent.click(screen.getByLabelText("SKILLS"))
+    expect((screen.getByLabelText("HOME") as HTMLInputElement).checked).toBe(false)
+    expect((screen.getByLabelText("SKILLS") as HTMLInputElement).checked).toBe(true)
   })
 })
