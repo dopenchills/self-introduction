1ffe08b74546101998b77d758d05adbd03873922
dopenchills <dopenchills@gmail.com>
2022-11-26 23:01:49 +0900

    refactor: separate LowerContainer scss from index

--- src/components/LowerContainer.tsx
+++ src/components/LowerContainer.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import MenuList from "./MenuList"
 import ContentsContainer from "./ContentsContainer"
-
+import './LowerContainer.scss'
 
 const LowerContainer = (): JSX.Element => {
 
